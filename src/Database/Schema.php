a85d9bb7ff7302cee6be8e59811066e1742482e1
Bernard Arhia <55264725+bernardarhia@users.noreply.github.com>
2022-06-01 05:37:32 +0000

    added rename functionality to the schema class

--- src/Database/Schema.php
+++ src/Database/Schema.php
@@ -3,7 +3,14 @@
 class Schema extends Table
 {
     static $table;
-    static function create($tableName, $callback)
+
+    /**
+     * @param string $tableName 
+     * The name of the database table you want to create
+     * @param callable $callback A callback function to define the properties of the table you want to create
+     * 
+     */
+    static function create(string $tableName, callable $callback)
     {
         self::$table = $tableName;
         $callback(new Table);
@@ -13,6 +20,16 @@ class Schema extends Table
     public function save()
     {
         print_r(self::$table);
+    }
+
+    /**
+     * 
+     * @param $from The current database table name you want to change
+     * @param $to The name of the new table you want to change the name to
+     */
+    public function rename(string $from, string $to)
+    {
+        $statement = "RENAME IF EXISTS `$from` TO `$to`";
     }
 }
 

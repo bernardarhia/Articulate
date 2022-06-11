4327e51aecf4f17f5ee4992c11655fc8d5e5dcb8
Bernard Arhia <55264725+bernardarhia@users.noreply.github.com>
2022-06-11 10:00:37 +0000

    removed routee from articluate

--- routes/user.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Controllers\Home;
-
-include_once __DIR__ . "/../controllers/HomeController.php";
-$router->get("/", [Home::class, "index"])->with(["name" => "John"]);
--- views/home.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <a href='/student/add'>Add User</a>
-</div>
-<h1>Home page</h1>
